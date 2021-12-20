05920cbdb142b13ecf453877a47fee6d11da1a44
Patryk Amsolik <amsithemlgpotato@gmail.com>
2021-12-20 23:26:18 +0100

    Updated dependencies.

--- Cars/Cars/Startup.cs
+++ Cars/Cars/Startup.cs
@@ -81,11 +81,11 @@ namespace Cars
                 //app.UseDeveloperExceptionPage();
                 app.UseMigrationsEndPoint();
             }
-            // else
-            // {
-            //     // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            //     app.UseHsts();
-            // }
+            else
+            {
+                // // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             app.ConfigureCustomExceptionMiddleware();
 
