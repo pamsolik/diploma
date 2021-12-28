afa31f5c7bdb0d8052068a3b919c8e3deaf10fc1
Patryk Amsolik <amsithemlgpotato@gmail.com>
2021-12-28 21:20:06 +0100

    Test 14

--- Cars/Cars/Startup.cs
+++ Cars/Cars/Startup.cs
@@ -16,7 +16,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Newtonsoft.Json.Converters;
 
 namespace Cars
 {
